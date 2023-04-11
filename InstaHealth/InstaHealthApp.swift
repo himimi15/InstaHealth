69234d3d45a0ea591a802d7753e24d8da358caa8
Michael Law <himimi15@gmail.com>
2023-04-10 22:02:21 -0400

    Initial Commit

---
+++ InstaHealth/InstaHealthApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaHealthApp.swift
+//  InstaHealth
+//
+//  Created by Michael Law on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct InstaHealthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
